7d8a59a32b87c9174cce62df27cc660e4e71424a
omarashzeinhom <omar.ashraf.zeinhom@hotmail.com>
2023-01-29 18:06:47 +0200

    initial-commit

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,7 +2,7 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
-
+import { markdownSchema } from "sanity-plugin-markdown";
 export default defineConfig({
   name: 'default',
   title: 'astroBackEnd',
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: 'tojmg0db',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [deskTool(), visionTool(),markdownSchema()],
 
   schema: {
     types: schemaTypes,
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,1 +1,7 @@
-export const schemaTypes = []
+import {article, author , categories} from "./types/index";
+
+export const schemaTypes = [
+  //article,
+  //author,
+  categories
+]
